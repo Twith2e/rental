3309c6b0345318ae27afa31f6e90146401f922b2
Twith2e <abdulbasitadebajo1619@gmail.com>
2025-09-18 05:37:26 +0100

    BE baseUrl fixed

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: "http://localhost:3000/",
+  // baseURL: "http://localhost:3000/",
+  baseURL: "rental-server-production-4619.up.railway.app",
   timeout: 10000,
 });
 
